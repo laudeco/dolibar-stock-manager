f6ed42a443e7585b894da9f24fac3a740028113c
Laurent De Coninck <lau.deconinck@gmail.com>
2020-01-14 10:42:52 +0100

    :sparkles: get the login from the query parameters

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,10 +16,13 @@ final class IndexController extends AbstractController
     /**
      * @Route("/", name="index", methods={"GET"})
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('index/index.html.twig');
+        $login = $request->get('l', $request->get('login', ''));
+        return $this->render('index/index.html.twig', ['login' => $login]);
     }
 }
