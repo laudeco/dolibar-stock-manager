18acc3389e7beea32aeda6b1b850dade2c55fa6b
Laurent De Coninck <lau.deconinck@gmail.com>
2020-02-03 12:48:38 +0100

    add sentry log

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class     => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class               => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class       => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class         => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
